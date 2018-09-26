e2703451b1e1be8c99875c7c32db9a5aeab81c4b
ConanKeaveney <32094186+ConanKeaveney@users.noreply.github.com>
2018-09-26 10:38:50 +0100

    Update backtest.py

--- backtest.py
+++ backtest.py
@@ -4,7 +4,8 @@ from botchart import BotChart
 from botstrategy import BotStrategy
 
 def main(argv):
-	chart = BotChart("poloniex","BTC-WAVES",300)
+	//change chart depending on exchange and Crypto Currency
+	//chart = BotChart("poloniex","BTC-WAVES",300)
 
 	strategy = BotStrategy()
 
