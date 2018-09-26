cb66b4dd3eed78ceecdd39e7d48762b4dff2fc4b
ConanKeaveney <32094186+ConanKeaveney@users.noreply.github.com>
2018-09-26 10:39:49 +0100

    Update backtest.py

--- backtest.py
+++ backtest.py
@@ -4,13 +4,13 @@ from botchart import BotChart
 from botstrategy import BotStrategy
 
 def main(argv):
-	//change chart depending on exchange and Crypto Currency
-	//chart = BotChart("poloniex","BTC-WAVES",300)
+	#change chart depending on exchange and Crypto Currency
+	#chart = BotChart("poloniex","BTC-WAVES",300)
 
 	strategy = BotStrategy()
 
-	for candlestick in chart.getPoints():
-		strategy.tick(candlestick)
+	#for candlestick in chart.getPoints():
+		#strategy.tick(candlestick)
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
